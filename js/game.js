48661c79b11d6c65a172280f6217b54500a5fc83
liuyj <975399405@qq.com>
2018-12-21 11:20:26 +0800

    增加怪兽移动功能

--- js/game.js
+++ js/game.js
@@ -8,6 +8,9 @@ document.body.appendChild(canvas);
 //全局变量
 //击杀怪兽数
 var monstersCaught = 0;
+//怪兽x，y轴的运动方向
+var monsterXStatus = 1;
+var monsterYStatus = 1;
 
 //加载图片
 //背景图
@@ -35,7 +38,7 @@ var hero = {
 };
 //怪兽
 var monster = {
-	speed: 256,//移动速度每秒
+	speed: 200,//移动速度每秒
 	image: monsterImage
 };
 
@@ -60,18 +63,30 @@ var reset = function () {
 };
 
 //更新英雄位置坐标
-var update = function (modifier) {
+var updateHeroXY = function (modifier) {
 	if (38 in keysDown) { //向上键
 		hero.y -= hero.speed * modifier;
+		if(hero.y < 0){
+			hero.y = 0;
+		}
 	}
 	if (40 in keysDown) { //向下键
 		hero.y += hero.speed * modifier;
+		if(hero.y > canvas.height-32){
+			hero.y = canvas.height-32;
+		}
 	}
 	if (37 in keysDown) { //向左键
 		hero.x -= hero.speed * modifier;
+		if(hero.x < 0){
+			hero.x = 0;
+		}
 	}
 	if (39 in keysDown) { //向右键
 		hero.x += hero.speed * modifier;
+		if(hero.x > canvas.width-32){
+			hero.x = canvas.width-32;
+		}
 	}
 
 	//判断是否图片碰撞
@@ -86,12 +101,47 @@ var update = function (modifier) {
 	}
 };
 
+//更新怪兽位置坐标
+var updateMonsterXY = function(){
+	var now = Date.now();
+	var runTime = (now - monsterChangeDate)/1000;
+	if(monsterXStatus == 1){
+		monster.x = monster.x + monster.speed*runTime;
+	}else{
+		monster.x = monster.x - monster.speed*runTime;
+	}
+	if(monster.x<0 || monster.x > canvas.width-32){
+		if(monster.x<0){
+			monster.x = 0
+		}else{
+			monster.x = canvas.width-32	
+		}
+		monsterXStatus = monsterXStatus * -1;
+	}
+	
+	if(monsterYStatus == 1){
+		monster.y = monster.y + Math.random();
+	}else{
+		monster.y = monster.y - Math.random();
+	}
+	if(monster.y<0 || monster.y > canvas.height-32){
+		if(monster.y<0){
+			monster.y = 0
+		}else{
+			monster.y = canvas.height-32;
+		}
+		monsterYStatus = monsterYStatus * -1;
+	}
+	monsterChangeDate = now;
+	requestAnimationFrame(updateMonsterXY);
+};
+
 //作画
 var render = function () {
 	ctx.drawImage(bgImage, 0, 0);
 	ctx.drawImage(hero.image, hero.x, hero.y);
 	ctx.drawImage(monster.image, monster.x, monster.y);
-	console.log(hero.image.src);
+	
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
@@ -104,7 +154,7 @@ var main = function () {
 	var now = Date.now();
 	var delta = now - then;
 	//更新英雄坐标
-	update(delta / 1000);
+	updateHeroXY(delta / 1000);
 	//作画
 	render();
 
@@ -134,6 +184,8 @@ requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame
 //开始游戏
 var then = Date.now();
 var heroChangeDate = Date.now();
+var monsterChangeDate = Date.now();
 reset();
 main();
 changeHeroImage();
+updateMonsterXY();
